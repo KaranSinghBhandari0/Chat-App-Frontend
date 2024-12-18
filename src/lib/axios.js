c551109ceb7f7e72c6c7ad1ba04589378784bce0
KaranSinghBhandari0 <kb632126@gmail.com>
2024-12-19 00:45:40 +0530

    updated backend url

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
     // baseURL: "http://localhost:3000/api",
-    baseURL : 'https://chat-app-backend-production-661f.up.railway.app/api',
+    baseURL : 'https://chat-appb.up.railway.app/api',
     withCredentials: true,
 });
